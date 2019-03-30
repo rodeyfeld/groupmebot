4f23bdae448331c4543e1f68336065fce508d4d8
rodeyfeld <eric.d.rodefeld@gmail.com>
2019-03-30 10:51:02 -0400

    Bot check for messages

--- bot/views.py
+++ bot/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 from .models import Bot
-# import requests
+import requests
 import os
 
 def index(request):
@@ -10,4 +11,17 @@ def index(request):
 def bot(request, groupme_bot_id):
     bot = Bot.objects.get(groupme_bot_id=groupme_bot_id)
     bot_name = bot.name
+    request_params = {
+        'token': settings.GROUPME_API_KEY
+    }
+
+    get_messages(bot, request_params)
+
     return HttpResponse("Bot page for %s" % bot_name)
+
+def get_messages(bot, request_params):
+    groupme_group_id = bot.groupme_group_id
+    response_messages = requests.get('https://api.groupme.com/v3/groups/' + groupme_group_id + '/messages', params = request_params).json()['response']['messages']
+    print(response_messages)
+
+    
