888253892486d4446a9b0856218231736f74938d
rodeyfeld <eric.d.rodefeld@gmail.com>
2019-04-11 18:34:02 -0400

    Locally testing gif command working

--- bot/views.py
+++ bot/views.py
@@ -14,15 +14,13 @@ def bot(request, groupme_bot_id):
     request_params = {
         'token': settings.GROUPME_API_KEY
     }
-    # print(bot_name)
     get_message(bot, request_params)
-
-    test_message(bot, request_params)
+    # test_message(bot, request_params)
     return HttpResponse("Bot page for %s" % bot_name)
 
 def test_message(bot, request_params):
-    most_recent_response = "!gif orca new token"
-    process_command(most_recent_response)
+    most_recent_response = "!gif super saiyan"
+    process_command(bot, most_recent_response)
 
 def get_message(bot, request_params):
     groupme_group_id = bot.groupme_group_id
@@ -33,10 +31,10 @@ def get_message(bot, request_params):
         most_recent_response = "!gif orca"
         if is_bot_command(most_recent_response):
             print("Bot has recieved a message")
-            process_command(most_recent_response)
+            process_command(bot, most_recent_response)
         else:
             print("User or bot has sent a message")
-            process_response(most_recent_response)
+            process_response(bot, most_recent_response)
     except:
         print("It failed bad")
 
@@ -46,13 +44,56 @@ def is_bot_command(response):
     if message[0] == '!' and sender_type != 'system':
         return True
 
-def process_command(message_response):
+def process_command(bot, message_response):
     command_tokens = message_response.split()
-    command = command_tokens[0].replace('!', '')
+    command = command_tokens[0].replace('!', '').upper()
     args = command_tokens[1:]
     print(command_tokens)
+
     print(command)
     print(args)
+    if command == 'GIF':
+        send_image_message(bot, ' '.join(args))
 
-def process_response(message_response):
+
+def process_response(bot, message_response):
     return True
+
+def get_giphy_image_url(search_term):
+    response = requests.get('https://api.giphy.com/v1/gifs/search', params={'api_key': os.environ.get('GIPHY_API_KEY', ''), 'q': search_term, 'limit': 1})
+    print(response.content)
+    giphy_image_url = response.json()['data'][0]['images']['original']['url']
+    return giphy_image_url
+
+def save_temp_image(url):
+    print(url)
+    response = requests.get(url, stream=True)
+    fpath =  "C:\\Users\\rodey\\PythonApps\\DjangoApps\\GroupMeBot\\bot\\tmp\\"
+    fname = "tmp.gif"
+    write_path = os.path.join(fpath, fname)
+    with open(write_path, 'wb') as writer:
+        for chunk in response:
+            writer.write(chunk)
+
+def get_image_service_url():
+    fpath =  "C:\\Users\\rodey\\PythonApps\\DjangoApps\\GroupMeBot\\bot\\tmp\\"
+    fname = "tmp.gif"
+    read_path = os.path.join(fpath, fname)
+    with open(read_path, 'rb') as reader:
+        reader_data = reader.read()
+    response = requests.post(url='https://image.groupme.com/pictures', data=reader_data, headers={'Content-Type': 'image/gif', 'X-Access-Token': os.environ.get('GROUPME_API_KEY', '')})
+    print(response)
+    print(response.content)
+    url = response.json()['payload']['url']
+    return url
+
+def send_image_message(bot, search_term):
+    groupme_bot_id = bot.groupme_bot_id
+    giphy_image_url = get_giphy_image_url(search_term)
+    save_temp_image(giphy_image_url)
+    image_service_url = get_image_service_url()
+    request_params = {'bot_id': groupme_bot_id, 'picture_url': image_service_url}
+    response = requests.post('https://api.groupme.com/v3/bots/post', params = request_params)
+    return response
+
+    
