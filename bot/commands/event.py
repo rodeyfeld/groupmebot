30e4ac7777feff4a56e4f2ca1acd68dfac9b834d
eric.d.rodefeld@gmail.com <eric.d.rodefeld@gmail.com>
2022-06-22 22:06:58 -0400

    Changed string

--- bot/commands/event.py
+++ bot/commands/event.py
@@ -9,7 +9,7 @@ from django.conf import settings
 
 def send_event(bot):
     print(bot.groupme_bot_id)
-    if bot.groupme_bot_id == '86257932':
+    if bot.groupme_bot_id == '6b8b4c8c1922a707fa53e5c4c0':
         quote_string = f"https://goo.gl/maps/cqUnVWxayqHiqBT68"
         request_params = {'bot_id': bot.groupme_bot_id, 'text': re.sub(r'\s+', ' ', quote_string)}
         # print(request_params)
